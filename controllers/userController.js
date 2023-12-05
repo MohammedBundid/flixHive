489a9c1c2e4d5f8e5eea266ae1d132d167fc1122
mohBundid <mohabdullahi2001@gmail.com>
2023-12-05 12:50:41 +0300

    added shows page

--- controllers/userController.js
+++ controllers/userController.js
@@ -38,13 +38,29 @@ const movies = async (req, res) => {
 
 }
 
-const tv_shows = (req, res) => {
+const tv_shows = async (req, res) => {
     // res.send('hello world');
-    const locals = {
-        title: "tv shows",
-        desc: "shows to watch"
+    try {
+        const locals = {
+            title: "shows for you to watch",
+            desc: "movies to watch",
+        }
+        
+        const data = await fetch(`https://api.themoviedb.org/3/tv/top_rated?api_key=${key}&language=en-US&page=1`)
+            .then(response => response.json())
+            .catch((error) => {
+                console.log("Error fetching data:", error.message)
+                res.status(500).send('Error fetching movie data');
+                throw error;
+            })
+
+        locals.shows = data.results
+
+    res.render('pages/shows', locals);
+
+    } catch (error) {
+        console.error("An unexpected error occurred:", error)
     }
-    res.render('movies', locals);
 }
 
 const movieById  = async (req, res) => {
@@ -69,6 +85,35 @@ const movieById  = async (req, res) => {
         };
 
     res.render('pages/movieViewer', locals)
+
+
+    } catch (error) {
+        console.error("An unexpected error occurred:", error);
+        res.status(500).send('Internal Server Error');
+    }
+}
+const showById  = async (req, res) => {
+
+    try {
+
+        const id = req.params.id;
+        const response = await fetch(`https://api.themoviedb.org/3/tv/${id}?api_key=${process.env,key}`)
+
+        if(!response.ok){
+            const errorData = await response.json();
+            console.error("Error from TMDb API:", errorData);
+            return res.status(response.status).send('Error from TMDb API');
+        }
+
+        const movie = await response.json();
+
+        const locals = {
+            title: "tv show",
+            desc: "movies to watch",
+            data: movie
+        };
+
+    res.render('pages/showViewer', locals)
 
 
     } catch (error) {
@@ -107,11 +152,60 @@ const movieTrailer  = async (req, res) => {
         res.status(500).send('Internal Server Error');
     }
 }
+const showTrailer  = async (req, res) => {
+
+    try {
+
+        const id = req.params.id;
+        const response = await fetch(`https://api.themoviedb.org/3/tv/${id}?api_key=${process.env.key}&append_to_response=videos
+        `)
+
+        if(!response.ok){
+            const errorData = await response.json();
+            console.error("Error from TMDb API:", errorData);
+            return res.status(response.status).send('Error from TMDb API');
+        }
+
+        const movie = await response.json();
+
+        const locals = {
+            title: "movie trailer",
+            desc: "movies to watch",
+            data: movie
+        };
+
+    res.render('pages/showTrailer', locals)
+
+
+    } catch (error) {
+        console.error("An unexpected error occurred:", error);
+        res.status(500).send('Internal Server Error');
+    }
+}
+
+const account = (req, res) => {
+    const locals = {
+        title: "coming soon",
+        desc: "coming soon",
+    };
+    res.render('pages/SOON.ejs', locals);
+}
+const register = (req, res) => {
+    const locals = {
+        title: "coming soon",
+        desc: "coming soon",
+    };
+    res.render('pages/SOON.ejs', locals);
+}
 
 module.exports = {
     // home,
     movies,
     tv_shows,
     movieById,
-    movieTrailer
+    movieTrailer,
+    showById,
+    showTrailer,
+    account,
+    register
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,4 +9,10 @@ router.get('/shows', userController.tv_shows)
 router.get('/movies/:id', userController.movieById)
 router.get('/movies/watch-trailer/:id', userController.movieTrailer)
 
+router.get('/shows/:id', userController.showById)
+router.get('/shows/watch-trailer/:id', userController.showTrailer)
+
+router.get('/account', userController.account);
+router.get('/register', userController.register);
+
 module.exports = router;
